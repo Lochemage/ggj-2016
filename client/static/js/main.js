cee25fd801fc92357ebc497b6c0d083d1e2b5bad
lochemage <lochemage@webcabin.org>
2016-02-06 00:28:30 -0800

    Restored the proper draw timer.

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var socket = io();
   var makingJudgement = false;
   var drawTimeoutId = 0;
-  var seconds = 4;
+  var seconds = 40;
 
   // Initialize the drawing canvas once.
   prepareCanvas();
